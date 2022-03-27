e12a93cec93137afbdcc3f2fd9cc652c5ac83874
Valerie Parham-Thompson <16852807+dataindataout@users.noreply.github.com>
2022-03-27 09:29:29 -0400

    Updates for ugprade of Python and dependencies

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Automating Girl Scout Events
 
-The shutdowns brought an opportunity for my daughter to participate in virtual scouting events all over the United States. When the registration form changed, I took the chance to try out some new web scraping skills while inspiring my daughter about the power of code for everyday tasks.
+The COVID shutdowns brought an opportunity for my daughter to participate in virtual scouting events all over the United States. When the registration form changed to limit searches to a 50 mile radius around any ZIP code, I took the chance to try out some new web scraping skills while inspiring my daughter about the power of code for everyday tasks.
 
 The solution includes Selenium, Beautiful Soup, and sqlite.
-
--- gs-events.py
+++ gs-events.py
@@ -8,34 +8,35 @@ from selenium.webdriver.chrome.options import Options
 
 from bs4 import BeautifulSoup
 import pandas as pd
-import sqlite3
 
 from urllib.parse import urlparse
 
 import hashlib
 
 from datetime import datetime
+import time
 
 from pangres import upsert
+
 from sqlalchemy import create_engine
 
-# retrieve and parse html 
+# retrieve and parse html
 
 options = Options()
 # options.headless = True
 options.add_argument("--window-size=1920,1200")
 
 # webdriver for Chrome
-DRIVER_PATH = '/Applications/chromedriver' 
+DRIVER_PATH = "/Applications/chromedriver"
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
 # add urls to list here as needed; some of our favorite councils for virtual events
-urls = [ \
-    "https://www.nccoastalpines.org/en/activities/activity-list.advanced.html", \
-    "https://www.girlscoutstoday.org/en/events/event-list.advanced.html",  \
-    "https://www.girlscoutsww.org/en/events/event-list.advanced.html",  \
-    "https://www.citrus-gs.org/en/events/event-list.advanced.html",  \
-        ]
+urls = [
+    "https://www.nccoastalpines.org/en/activities/activity-list.advanced.html",
+    "https://www.gsle.org/en/events/event-list.advanced.html",
+    "https://www.girlscoutstoday.org/en/events/event-list.advanced.html",
+    "https://www.citrus-gs.org/en/events/event-list.advanced.html",
+]
 
 # initiate list buckets
 links = []
@@ -47,59 +48,75 @@ uniquekeys = []
 
 # loop through urls, process form, and get data from page
 for url in urls:
-    # click and submit
-    driver.get(url)
-    driver.find_element_by_id("sf-activities:program-level/seniors").click()
-    driver.find_element_by_id("sub").submit()
 
-    # here's the page, make the soup
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'lxml')
+    try:
+        # click and submit
+        driver.get(url)
+        driver.find_element_by_xpath(
+            "//input[contains(translate(@id, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'program-level/ambassador')]"
+        ).click()
+        driver.find_element_by_id("sub").submit()
 
-    # get council identifier from url
-    council = urlparse(url).netloc.split(".")[1]
+        # here is the page, make the soup
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, "lxml")
 
-    # now get the data on each page
-    for x in soup.find_all(class_='eventsList eventSection'):
+        # get council identifier from url
+        council = urlparse(url).netloc.split(".")[1]
+        primarylink = urlparse(url).netloc
 
-        link = ""
-        title = ""
-        startdate = ""
-        enddate = ""
+        # now get the data on each page
+        for x in soup.find_all(class_="eventsList eventSection"):
 
-        link = url + x.h6.a["href"]
-        links.append(link)
+            link = ""
+            title = ""
+            startdate = ""
+            enddate = ""
 
-        title = x.h6.a.text
-        titles.append(title)
+            link = primarylink + x.h6.a["href"]
+            links.append(link)
 
-        startdate = x.find_next("span", itemprop="startDate")['content']
-        startdates.append(startdate)
+            title = x.h6.a.text
+            titles.append(title)
 
-        enddate = x.find_next("span", itemprop="stopDate")['content']
-        enddates.append(enddate)
+            startdate = x.find_next("span", itemprop="startDate")["content"]
+            startdates.append(startdate)
 
-        councils.append(council)
+            enddate = x.find_next("span", itemprop="stopDate")["content"]
+            enddates.append(enddate)
 
-        uniquekey = hashlib.sha224(str(link + startdate).encode('utf-8')).hexdigest()
-        uniquekeys.append(uniquekey)
+            councils.append(council)
+
+            uniquekey = hashlib.sha224(
+                str(link + startdate).encode("utf-8")
+            ).hexdigest()
+            uniquekeys.append(uniquekey)
+
+        time.sleep(5)
+
+    except Exception as e:
+        print("An error has occurred")
+        print(e)
+        continue
 
 driver.quit()
 
 # put data in a dataframe
-# don't strictly need a df here, except pangres uses it
+# a df not strictly needed here, except pangres uses it
 
-df = pd.DataFrame({
-'uniquekey':uniquekeys,
-'link':links,
-'title':titles,
-'startdate': startdates,
-'enddate': enddates,
-'council': councils,
-})
+df = pd.DataFrame(
+    {
+        "uniquekey": uniquekeys,
+        "link": links,
+        "title": titles,
+        "startdate": startdates,
+        "enddate": enddates,
+        "council": councils,
+    }
+)
 
 # set an index, as code below will need it; this also removes default ID
-df.set_index('uniquekey', inplace=True)
+df.set_index("uniquekey", inplace=True)
 
 # pangres / sqlalchemy upsert
 
@@ -110,16 +127,12 @@ df.set_index('uniquekey', inplace=True)
 #   "startdate" TEXT,
 #   "enddate" TEXT,
 #   "council" TEXT,
-#   "updateddate" DATETIME DEFAULT CURRENT_TIMESTAMP 
+#   "updateddate" DATETIME DEFAULT CURRENT_TIMESTAMP
 # );
 
-engine = create_engine('sqlite:////tmp/gs.sqlite')
+engine = create_engine("sqlite:////tmp/gs.sqlite")
 
-upsert(engine=engine,
-       df=df,
-       table_name='events',
-       if_row_exists='update',
-       dtype=None)
+upsert(con=engine, df=df, table_name="events", if_row_exists="update", dtype=None)
 
 # delete from database where enddate < today (this really should be registration date, when available)
 
@@ -129,4 +142,3 @@ deleteExpiredSQL = f"delete from events where enddate<'{str(datetime.now())}'"
 
 with engine.connect() as connection:
     result = connection.execute(text(deleteExpiredSQL))
-
